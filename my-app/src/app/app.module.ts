da88b7cb4d63795b087daa927838306d6e88a966
Ido <[200~123030220+talxdo@users.noreply.github.com~>
2023-11-30 16:29:13 +0100

    creacion de componentes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecetaComponent } from './receta/receta.component';
+import { ListaRecetasComponent } from './lista-recetas/lista-recetas.component';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
+import { DetallesRecetaComponent } from './detalles-receta/detalles-receta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecetaComponent,
+    ListaRecetasComponent,
+    BarraNavegacionComponent,
+    DetallesRecetaComponent
   ],
   imports: [
     BrowserModule
